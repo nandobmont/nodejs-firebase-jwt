ee75893d0f61e96063121dacefe5ec375a428ca0
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2022-06-15 13:31:58 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ const firebase = require("firebase-admin");
 
 firebase.initializeApp({
   credential: firebase.credential.cert(
-    require("../files/service_account_credentials.json")
+    require("../files/service_account_credentials.jsonc")
   )
 });
 
